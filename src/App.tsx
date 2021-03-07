ef59511fa1e30e2822e6ead28b6471a42dd10646
Domi Roger <domitille.roger@gmail.com>
2021-03-07 18:40:58 +0100

    fix(instructions): clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.scss';
-import jsonPattern from "./patterns/trescao.json";
-import { formatJSON } from "./utils/Pattern";
-
-import Part from "./components/Part/Part";
-
-const defaultCurrentSize = -10;
-const currentSizeToDev = 105;
+import Instructions from "./components/Instructions/Instructions";
 
 function App() {
-const currentPattern = formatJSON(jsonPattern);
-  const [currentSize, setCurrentSize] = useState(currentPattern.sizes.indexOf(currentSizeToDev));
-
-  function onChangeSelect(event:any) {
-    event.target.value === "all"
-      ? setCurrentSize(defaultCurrentSize)
-      : setCurrentSize(currentPattern.sizes.indexOf(parseInt(event.target.value)));
-  }
-
   return (
     <div className="knittingApp">
       <div className="pattern">
@@ -29,23 +14,9 @@ const currentPattern = formatJSON(jsonPattern);
         </div>
         <div className="pattern-instructions">
           <h2>instructions</h2>
+          <Instructions />
         </div>
       </div>
-      {/*<header className="App-header">
-        <h1>{currentPattern.name}</h1>
-      </header>
-      <main>
-        <label>Choisir la taille : </label>
-        <select name="sizes" id="size-select" onChange={onChangeSelect}>
-          <option value={-1}>All</option>
-          {currentPattern.sizes.map(size =>
-            <option key={size} value={size} selected={size===currentSizeToDev}>{size}</option>
-          )}
-        </select>
-        <div className="pattern">
-          {currentPattern.part.map((part, index) => <Part key={index} part={part} currentSize={currentSize}/>)}
-        </div>
-      </main>*/}
     </div>
   );
 }
---
+++ src/components/Instructions/Instructions.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import '../../App.scss';
+import jsonPattern from "../../patterns/trescao.json";
+import { formatJSON } from "../../utils/Pattern";
+
+import Part from "../../components/Part/Part";
+
+const defaultCurrentSize = -10;
+const currentSizeToDev = 105;
+
+function Instructions() {
+  const currentPattern = formatJSON(jsonPattern);
+  const [currentSize, setCurrentSize] = useState(currentPattern.sizes.indexOf(currentSizeToDev));
+
+  function onChangeSelect(event:any) {
+    event.target.value === "all"
+      ? setCurrentSize(defaultCurrentSize)
+      : setCurrentSize(currentPattern.sizes.indexOf(parseInt(event.target.value)));
+  }
+
+  return (
+    <div className="instructions">
+      <label>Choisir la taille : </label>
+      <select name="sizes" id="size-select" onChange={onChangeSelect}>
+        <option value={-1}>All</option>
+        {currentPattern.sizes.map(size =>
+          <option key={size} value={size} selected={size===currentSizeToDev}>{size}</option>
+        )}
+      </select>
+      <div className="part">
+        {currentPattern.part.map((part, index) => <Part key={index} part={part} currentSize={currentSize}/>)}
+      </div>
+    </div>
+  );
+}
+
+export default Instructions;
